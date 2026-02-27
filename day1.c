eb32ae5621793ee34bdedc9503465a31d260ca21
VEDANSH005 <rawatvedans@gmail.com>
2026-02-27 12:26:45 +0530

    code

--- day1.c
+++ day1.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, pos, x;
+    int arr[100];  
+
     scanf("%d", &n);
 
-    int arr[100];  // assume max size 100
-
-    // Input array elements
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    int pos, x;
     scanf("%d", &pos);
+
     scanf("%d", &x);
 
-    // Shift elements to the right
     for(int i = n; i >= pos; i--) {
         arr[i] = arr[i - 1];
     }
 
-    // Insert element
     arr[pos - 1] = x;
 
-    // Print updated array
     for(int i = 0; i <= n; i++) {
         printf("%d ", arr[i]);
     }
